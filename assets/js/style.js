2bc76c9419a2d1e01cbbe6f301785b99b69c919c
Danylo <danil.zinchenko2018@gmail.com>
2025-06-16 00:30:28 +0300

    Add project

--- assets/js/style.js
+++ assets/js/style.js
@@ -9,4 +9,26 @@ array.shift();
 console.table(array);
 console.log(array.length);
 const array2 = [...array];
-Ё;
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+        console.log(array[i]);
+    }
+}
+let multiply = 1;
+for (let i = 1; i < array.length; i++) {
+    multiply *= array[i];
+}
+console.log(multiply);
+
+const notNull = array.filter((a) => a !== 0);
+console.log(notNull);
+const sum = array.map((a) => a / 100);
+console.log(sum);
+const cube = array.map((a) => a ** 2);
+array.forEach((a) => console.log(a));
+const index = array.findIndex((a) => a === 100);
+if (index !== -1) {
+    array.splice(index, 1);
+} else {
+    console.log("Такого Індекса не існеує");
+}
